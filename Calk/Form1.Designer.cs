3acb4db51f18a6a6fc35ff08b572d1cefc7ccc55
CppKotlin <cscppkotlin@gmail.com>
2024-04-18 23:00:34 +0300

    все ок

--- Calk/Form1.Designer.cs
+++ Calk/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calk
 {
-    partial class Form1
+    partial class Калькулятор
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,368 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem = new ToolStripMenuItem();
+            выходToolStripMenuItem = new ToolStripMenuItem();
+            оПрограммеToolStripMenuItem = new ToolStripMenuItem();
+            groupBox1 = new GroupBox();
+            button13 = new Button();
+            button12 = new Button();
+            button11 = new Button();
+            button10 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            button20 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button17 = new Button();
+            button16 = new Button();
+            button15 = new Button();
+            button14 = new Button();
+            textBox1 = new TextBox();
+            menuStrip1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, оПрограммеToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(582, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { выходToolStripMenuItem });
+            файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            файлToolStripMenuItem.Size = new Size(59, 24);
+            файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            выходToolStripMenuItem.Size = new Size(136, 26);
+            выходToolStripMenuItem.Text = "Выход";
+            выходToolStripMenuItem.Click += выходToolStripMenuItem_Click;
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            оПрограммеToolStripMenuItem.Size = new Size(118, 24);
+            оПрограммеToolStripMenuItem.Text = "О программе";
+            оПрограммеToolStripMenuItem.Click += оПрограммеToolStripMenuItem_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button13);
+            groupBox1.Controls.Add(button12);
+            groupBox1.Controls.Add(button11);
+            groupBox1.Controls.Add(button10);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button9);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(12, 82);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(334, 309);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(95, 23);
+            button13.Name = "button13";
+            button13.Size = new Size(64, 44);
+            button13.TabIndex = 12;
+            button13.Text = "del";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(255, 192, 192);
+            button12.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button12.Location = new Point(19, 23);
+            button12.Name = "button12";
+            button12.Size = new Size(70, 44);
+            button12.TabIndex = 11;
+            button12.Text = "C";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(171, 244);
+            button11.Name = "button11";
+            button11.Size = new Size(140, 47);
+            button11.TabIndex = 10;
+            button11.Text = "00";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(18, 244);
+            button10.Name = "button10";
+            button10.Size = new Size(140, 47);
+            button10.TabIndex = 9;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(19, 181);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 56);
+            button7.TabIndex = 8;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(118, 181);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 56);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(218, 181);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 56);
+            button9.TabIndex = 6;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(19, 130);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 45);
+            button4.TabIndex = 5;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(118, 130);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 45);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(218, 130);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 45);
+            button6.TabIndex = 3;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(218, 73);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 51);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(118, 73);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 51);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(18, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 51);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button20);
+            groupBox2.Controls.Add(button18);
+            groupBox2.Controls.Add(button19);
+            groupBox2.Controls.Add(button17);
+            groupBox2.Controls.Add(button16);
+            groupBox2.Controls.Add(button15);
+            groupBox2.Controls.Add(button14);
+            groupBox2.Location = new Point(352, 142);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(202, 249);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(22, 124);
+            button20.Name = "button20";
+            button20.Size = new Size(79, 106);
+            button20.TabIndex = 6;
+            button20.Text = "=";
+            button20.UseVisualStyleBackColor = true;
+            button20.Click += button20_Click;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(107, 186);
+            button18.Name = "button18";
+            button18.Size = new Size(79, 44);
+            button18.TabIndex = 5;
+            button18.Text = "SQRT";
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += button18_Click;
+            // 
+            // button19
+            // 
+            button19.Location = new Point(107, 124);
+            button19.Name = "button19";
+            button19.Size = new Size(79, 56);
+            button19.TabIndex = 4;
+            button19.Text = ",";
+            button19.UseVisualStyleBackColor = true;
+            button19.Click += button19_Click;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(107, 73);
+            button17.Name = "button17";
+            button17.Size = new Size(79, 45);
+            button17.TabIndex = 3;
+            button17.Text = "/";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += button17_Click;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(22, 73);
+            button16.Name = "button16";
+            button16.Size = new Size(79, 45);
+            button16.TabIndex = 2;
+            button16.Text = "x";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(107, 19);
+            button15.Name = "button15";
+            button15.Size = new Size(79, 45);
+            button15.TabIndex = 1;
+            button15.Text = "-";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(22, 19);
+            button14.Name = "button14";
+            button14.Size = new Size(79, 45);
+            button14.TabIndex = 0;
+            button14.Text = "+";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Sitka Banner", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(33, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(521, 45);
+            textBox1.TabIndex = 3;
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // Калькулятор
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(582, 403);
+            Controls.Add(textBox1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            Name = "Калькулятор";
+            Text = "Калькулятор";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem выходToolStripMenuItem;
+        private ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private GroupBox groupBox1;
+        private Button button13;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox2;
+        private Button button20;
+        private Button button18;
+        private Button button19;
+        private Button button17;
+        private Button button16;
+        private Button button15;
+        private Button button14;
+        private TextBox textBox1;
     }
 }
--- Calk/Form1.cs
+++ Calk/Form1.cs
@@ -1,10 +1,164 @@
 namespace Calk
 {
-    public partial class Form1 : Form
+    public partial class Калькулятор : Form
     {
-        public Form1()
+        public Калькулятор()
         {
             InitializeComponent();
+        }
+
+        public double a;
+        public double b;
+        public char c;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "00";
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            c = '+';
+            textBox1.Text = "";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            c = '-';
+            textBox1.Text = "";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            c = '*';
+            textBox1.Text = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            c = '/';
+            textBox1.Text = "";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(textBox1.Text);
+
+            textBox1.Text = "";
+            switch (c)
+            {
+                case '+': textBox1.Text = Convert.ToString(a + b); break;
+                case '-': textBox1.Text = Convert.ToString(a - b); break;
+                case '*': textBox1.Text = Convert.ToString(a * b); break;
+                case '/':
+                    if (b != 0) { textBox1.Text = Convert.ToString(a / b); }
+                    else textBox1.Text = "На ноль делить нельзя!     ";
+                    break;
+
+                default:
+                    textBox1.Text = "Error"; break;
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ",";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = Convert.ToString(Math.Sqrt(a));
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            a = 0; b = 0;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            try
+            {
+                s = s.Substring(0, s.Length - 1);
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+            textBox1.Text = s;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Калькулятор от Ивана", "О программе");
         }
     }
 }
